d7a1821424748c5a429f9d2d204a930ab166c14c
Vincent <vincentkok@live.nl>
2018-06-03 19:40:10 +0200

    Increased sample size and included history

--- Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/Rock_Paper_Scissors.py
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/Rock_Paper_Scissors.py
@@ -8,10 +8,11 @@ dataset = pd.read_csv('Rock_Paper_Scissors.csv')
 
 # Implement upper confidence bound
 import math
-number_of_rows = 192
+number_of_rows = len(dataset)
 number_of_options = 3
 number_of_selections = [0] * number_of_options
 options_rewards = [0] * number_of_options
+history = [0] * number_of_rows
 
 # Loop through the rows in our dataset
 for n in range(0, number_of_rows):    
@@ -19,7 +20,7 @@ for n in range(0, number_of_rows):
     max_upper_bound = 0
     # Loop through the number of options we have
     for i in range(0, number_of_options):
-        # If this ad has been shown to a user before, calculate the upper bound
+        # If this option has been used in a game before, calculate the upper bound
         if number_of_selections[i] > 0:
             # Calculate the average reward up to this point
             average_reward = options_rewards[i] / number_of_selections[i]
@@ -38,5 +39,6 @@ for n in range(0, number_of_rows):
     # Check the dataset to see if we get a reward
     reward = dataset.values[n, ad] 
     options_rewards[ad] = options_rewards[ad] + reward
+    history[n] = ad
 
 total_reward = sum(options_rewards)
