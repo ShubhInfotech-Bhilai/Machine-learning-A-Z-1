7d18f94fa0fd0d516fc31f661e79ecc8fd28af24
Vincent <vincentkok@live.nl>
2018-06-10 15:59:46 +0200

    Implemented a simple linear regression model to predict salaries given a dataset

--- Part 2 - Regression/Section 4 - Simple Linear Regression/NET/Program.cs
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/NET/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Accord.Statistics.Models.Regression.Linear;
+using CsvHelper;
+using System;
 
 namespace SimpleLinearRegression {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
+            IEnumerable<SalaryDataRow> dataset = ReadDataset();
+
+            // Set the independant variable
+            double[] inputs = dataset.Select(x => x.YearsExperience).ToArray();
+
+            // Set the dependant variables
+            double[] outputs = dataset.Select(x => x.Salary).ToArray();
+
+            // Train the model
+            OrdinaryLeastSquares ols = new OrdinaryLeastSquares();
+            Accord.Statistics.Models.Regression.Linear.SimpleLinearRegression regression = ols.Learn(inputs, outputs);
+
+            // Ask the user for a prediction
+            while (true) {
+                Console.WriteLine("Enter the years of experience: ");
+                string userInput = Console.ReadLine();
+                double yearsOfExperience;
+                if (double.TryParse(userInput, out yearsOfExperience)) {
+                    double predictedSalary = regression.Transform(yearsOfExperience);
+                    Console.WriteLine($"I predict the salary will be: {predictedSalary}");
+                }
+                else {
+                    Console.WriteLine("Please enter a valid number");
+                }
+            }
         }
+
+        public static IEnumerable<SalaryDataRow> ReadDataset() {
+            using (TextReader textReader = File.OpenText("Dataset/Salary_Data.csv")) {
+                var csv = new CsvReader(textReader);
+                return csv.GetRecords<SalaryDataRow>().ToList();
+            }
+        }
+    }
+
+    public class SalaryDataRow {
+        public double YearsExperience { get; set; }
+        public double Salary { get; set; }
     }
 }
