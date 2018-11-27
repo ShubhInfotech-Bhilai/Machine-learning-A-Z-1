50967a54523fcd5f970743e986d28780360dead1
Vincent <vincentkok@live.nl>
2018-11-27 20:07:10 +0100

    Added code from ML.NEt tutorial

--- Part 7 - Natural Language Processing/ML.NET/LanguageProcessing/Program.cs
+++ Part 7 - Natural Language Processing/ML.NET/LanguageProcessing/Program.cs
@@ -10,8 +10,72 @@ using Microsoft.ML.Transforms.Text;
 
 namespace LanguageProcessing {
     class Program {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "wikipedia-detox-250-line-data.tsv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "wikipedia-detox-250-line-test.tsv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+        static TextLoader _textLoader;
+
         static void Main(string[] args) {
-            
+            MLContext mlContext = new MLContext(seed: 0);
+
+            _textLoader = mlContext.Data.TextReader(new TextLoader.Arguments() {
+                    Separator = "tab",
+                    HasHeader = true,
+                    Column = new[] {
+                        new TextLoader.Column("Label", DataKind.Bool, 0),
+                        new TextLoader.Column("SentimentText", DataKind.Text, 1)
+                    }
+                }
+            );
+
+            var model = Train(mlContext, _trainDataPath);
+            Evaluate(mlContext, model);
+            Predict(mlContext, model);
+
+            Console.Read();
+        }
+
+        public static ITransformer Train(MLContext mlContext, string dataPath) {
+            IDataView dataView = _textLoader.Read(dataPath);
+            var pipeline = mlContext.Transforms.Text.FeaturizeText("SentimentText", "Features")
+                .Append(mlContext.BinaryClassification.Trainers.FastTree(numLeaves: 50, numTrees: 50, minDatapointsInLeafs: 20));
+
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = pipeline.Fit(dataView);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+
+            return model;
+        }
+
+        public static void Evaluate(MLContext mlContext, ITransformer model) {
+            IDataView dataView = _textLoader.Read(_testDataPath);
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            var predictions = model.Transform(dataView);
+            var metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.Auc:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+
+        private static void Predict(MLContext mlContext, ITransformer model) {
+            var predictionFunction = model.MakePredictionFunction<SentimentData, SentimentPrediction>(mlContext);
+            SentimentData sampleStatement = new SentimentData {
+                SentimentText = "This is a very rude movie"
+            };
+            var resultprediction = predictionFunction.Predict(sampleStatement);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {sampleStatement.SentimentText} | Prediction: {(Convert.ToBoolean(resultprediction.Prediction) ? "Toxic" : "Not Toxic")} | Probability: {resultprediction.Probability} ");
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
         }
     }
 }
