7ed18c1ecbf393a1495018f2ff2e0421f28e79fd
Vincent <vincentkok@live.nl>
2018-11-27 19:57:48 +0100

    Adding ML.NET console application

---
+++ Part 7 - Natural Language Processing/ML.NET/LanguageProcessing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Core.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Transforms.Text;
+
+namespace LanguageProcessing {
+    class Program {
+        static void Main(string[] args) {
+            
+        }
+    }
+}
